1796ab9dcba7bfcb021054aa1b1ea71338f60969
Lucas Lima <luycaslima@gmail.com>
2023-06-13 22:01:07 +0200

    fix: remove of appollo lib

--- src/lib/apollo.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient } from "@apollo/client";
-import { InMemoryCache } from "@apollo/client/cache";
-
-export const client = new ApolloClient({
-    uri:'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cla3tiut634e701uk9sena3eu/master',
-    cache: new InMemoryCache()
-})
